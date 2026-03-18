c830f702c9c3176effdb930bb92b88ecb3f823bb
Venkata Sai Pranavi <pranusarala@gmail.com>
2026-03-18 21:21:35 +0530

    Delete regex_debug.py

--- regex_debug.py
+++
@@ -1,4 +0,0 @@
-import re
-line = "MbBTO TENT 46.30"
-match = re.search(r"\b(?:total|tent|t0tal)\b[^0-9]*(\d+[\.,]\d{2})", line, re.I)
-print('match', match, 'group', match.group(1) if match else None)
